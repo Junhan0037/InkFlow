7573208b705fb6baa2ad0e2d76b480d1cd6d9ea5
Junhan0037 <wnsgks0037@naver.com>
2026-01-05 14:55:31 +0900

    Initial commit

---
+++ src/test/kotlin/com/inkflow/InkFlowApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.inkflow
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class InkFlowApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
